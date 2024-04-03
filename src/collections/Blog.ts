82881130cd78225d089f8724b7ad2ad5496684e8
ryann254 <keddelyronjoz@gmail.com>
2024-04-03 16:42:59 +0300

    feat: add access-control to blog data

--- src/collections/Blog.ts
+++ src/collections/Blog.ts
@@ -1,7 +1,19 @@
 import { CollectionConfig } from 'payload/types';
+import { isAdmin } from '../access/isAdmin';
+import { isAdminOrSelf } from '../access/isAdminOrSelf';
 
 const Blog: CollectionConfig = {
   slug: 'blog',
+  access: {
+    // Only admins can create users
+    create: isAdminOrSelf,
+    // Admins can read all, but any other logged in user can only read themselves
+    read: isAdminOrSelf,
+    // Admins can update all, but any other logged in user can only update themselves
+    update: isAdminOrSelf,
+    // Only admins can delete
+    delete: isAdmin,
+  },
   fields: [
     {
       name: 'title',
