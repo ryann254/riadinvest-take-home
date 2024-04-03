ca0a431729573946ad412de42addf7a1657fb602
ryann254 <keddelyronjoz@gmail.com>
2024-04-03 11:14:29 +0300

    feat: add required features for take home test

---
+++ src/collections/Blog.ts
@@ -0,0 +1,31 @@
+import { CollectionConfig } from 'payload/types';
+
+const Blog: CollectionConfig = {
+  slug: 'blog',
+  auth: true,
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'author',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'date',
+      type: 'date',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+    },
+  ],
+};
+
+export default Blog;
---
+++ src/collections/index.ts
@@ -0,0 +1,4 @@
+import Users from './Users';
+import Blog from './Blog';
+
+export { Users, Blog };
